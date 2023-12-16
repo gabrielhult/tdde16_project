41f73e74b1838aacd20233afa29fad8b12be1c89
Gabriel Hult <hultgabriel@live.se>
2023-12-16 23:47:30 +0100

    More VADER tweaks, still fixes to be made

--- add_sentiment.py
+++ add_sentiment.py
@@ -20,11 +20,27 @@ def add_sentiment():
     # Add sentiment data to sentiment column
     for i in range(len(data)):
         sentiment_scores = analyser.polarity_scores(data['Title'][i])
-        data.at[i, 'sentiment'] = str(sentiment_scores)  # Convert dictionary to string
+        max_sentiment = max(sentiment_scores, key=sentiment_scores.get)
+        if max_sentiment == 'pos':
+            data.at[i, 'sentiment'] = 'Positive'
+        elif max_sentiment == 'neg':
+            data.at[i, 'sentiment'] = 'Negative'
+        else:
+            data.at[i, 'sentiment'] = 'Neutral'
 
     # Save data
     sentiment_file_name = 'sentiment_posts.csv'
     data.to_csv(sentiment_file_name, index=False)
+
+    # Count the amount of neutral, negative, and positive sentiments
+    neutral_count = len(data[data['sentiment'] == 'Neutral'])
+    negative_count = len(data[data['sentiment'] == 'Negative'])
+    positive_count = len(data[data['sentiment'] == 'Positive'])
+
+    # Print the counts
+    print("Neutral count:", neutral_count)
+    print("Negative count:", negative_count)
+    print("Positive count:", positive_count)
 
     # Plot sentiment
     # plt.plot(data['sentiment'])
